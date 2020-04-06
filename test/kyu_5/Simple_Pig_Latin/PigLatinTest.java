0f34a5c93d89ba52e8760bf1670ef736da521316
Enigma <enigma3420@gmail.com>
2020-04-06 13:03:26 +0200

    Solved Kata Task => Kyu 4

--- test/kyu_5/Simple_Pig_Latin/PigLatinTest.java
+++ test/kyu_5/Simple_Pig_Latin/PigLatinTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class PigLatinTest {
-    
+
     @Test
     public void FixedTests() {
         assertEquals("igPay atinlay siay oolcay", PigLatin.pigIt("Pig latin is cool"));
