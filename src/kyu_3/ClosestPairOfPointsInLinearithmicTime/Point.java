304825962d1a13279d1acc945fbfd562e096d504
Enigma <enigma3420@gmail.com>
2020-04-06 16:04:24 +0200

    Solved Kata Task => Kyu 3

---
+++ src/kyu_3/ClosestPairOfPointsInLinearithmicTime/Point.java
@@ -0,0 +1,50 @@
+package kyu_3.ClosestPairOfPointsInLinearithmicTime;
+
+public class Point {
+    public double x, y;
+
+    public Point() {
+        x = y = 0.0;
+    }
+
+    public Point(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double getX() {
+        return x;
+    }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public double getY() {
+        return y;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%f, %f)", x, y);
+    }
+
+    @Override
+    public int hashCode() {
+        return Double.hashCode(x) ^ Double.hashCode(y);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj instanceof Point) {
+            Point other = (Point) obj;
+            return x == other.x && y == other.y;
+        } else {
+            return false;
+        }
+    }
+}
